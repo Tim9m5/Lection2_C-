95cca4496da30c883e201fc417a546fb0cd8fae3
Timur Halitov <halitovtimur@list.ru>
2023-02-16 16:10:56 +0500

    Final commit

--- Lection2_C#/Find/Program.cs
+++ Lection2_C#/Find/Program.cs
@@ -25,7 +25,7 @@ void FillArray(int[] collection)
 {
     int length = collection.Length;
     int index = 0;
-    while (index<length)
+    while (index < length)
     {
         collection[index] = new Random().Next(1, 10);
         index++;
@@ -43,7 +43,33 @@ void PrintArray(int[] col)
     }
 }
 
-int [] array = new int[10]; // new int [n] - создать новый массив в котором будет n элементов, по умолчанию нули
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // если мы ищем элемент которого точно не существует, например 444
+    // то программа выдаст нам значение position по умолчанию т.е. -1, если мы поставим 
+    // int position = 0 то будет выдываться 0 и будет непонятно почему он выдал 0, хотя значение 
+    // в ячейке с индексом 0 другое. 
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
 
-FillArray (array);
-PrintArray (array);
+int[] array = new int[10]; // new int [n] - создать новый массив в котором будет n элементов, по умолчанию нули
+
+FillArray(array); // заполнить массив
+array[4] = 4; // принудительно(искусственно) добавили 4 на 4 позицию
+array[6] = 4; // принудительно(искусственно) добавили 4 на 6 позицию
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
